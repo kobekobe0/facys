0c19a6ec9019cf89a84930636c58a2b985244e74
Kobe Brian Santos <kobe0santos0@gmail.com>
2024-12-02 05:12:04 +0800

    time

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-//const API_URL = 'https://facysserver-production.up.railway.app/api/';
-const API_URL = 'http://localhost:4000/api/';
+const API_URL = 'https://facysserver-production.up.railway.app/api/';
+//const API_URL = 'http://localhost:4000/api/';
 
 export default API_URL;
